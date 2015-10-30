52e4311421996492554552c9ff34c9bbd2931933
cew <cewillismail@gmail.com>
2015-10-30 10:33:44 -0400

    implimented most things, still need timeout support

--- packet.go
+++ packet.go
@@ -6,6 +6,17 @@ import (
 	"fmt"
 )
 
+type File struct {
+	Name string
+	Data []byte
+	Conn *net.UDPConn
+	Raddr *net.UDPAddr
+}
+
+func min(a int, b int) int{
+	if(a <= b) { return a } else { return b }
+}
+
 func checkError(err error){
 	if (err != nil) {
 		fmt.Println("Error: ", err)
@@ -15,23 +26,62 @@ func checkError(err error){
 func makeRRQ(filename string) ([]byte, error ){
 	packet, err := makeWRQ(filename)
 	if(packet != nil){ 
-		packet[1] = 1;
+		packet[1] = 1
 	}
 	return packet, err
 }
 
 func SendRRQ(filename string, Conn *net.UDPConn){
 	msg, err := makeRRQ(filename)
-	checkError(err);
+	checkError(err)
 	_,err = Conn.Write(msg)
-	checkError(err);
+	checkError(err)
 }
 
-SendRRQTo(filename string, Conn *net.UDPConn){
+func SendRRQTo(filename string, Conn *net.UDPConn, raddr *net.UDPAddr){
   msg, err := makeRRQ(filename)
-  checkError(err);
-  _,err = Conn.WriteTo(msg)
-  checkError(err);
+  checkError(err)
+  _,err = Conn.WriteTo(msg, raddr)
+  checkError(err)
+}
+
+func HandleRRQ(filename string, in <-chan []byte, Conn *net.UDPConn, raddr *net.UDPAddr, f []byte) {
+	index := 0
+	max := min(512, len(f))
+	data := f[index : max]
+	block := []byte{0, 1}
+	SendDATATo(block, data, Conn, raddr)
+
+	for r := range in {
+		//if timeout, resend last data
+		//if ack, send new data
+		ackBlock, err := GetAckBlock(r)
+		checkError(err)
+		if(ackBlock[1] == block[1] && len(data) >= 512){
+			index += 512
+			max = min(index + 512, len(f))
+			data = f[index : max]
+			block[1]++
+			SendDATATo(block, data, Conn, raddr)
+		} else if (len(data) < 512){
+			break
+		} else{
+			//handle erroneous ackknowledgements
+		}
+	}	
+	//hang around for ack block, if none before timeout, resend data
+}
+
+func GetRRQname(p []byte) (string, error){
+  if(len(p) < 9){ //opcode + 0 + octet + 0
+    return "", errors.New("badly formed RRQ packet")
+  }
+  index := 0
+  for i := 2; i < len(p); i++ {
+    if p[i] == 0 { index = i; break  }
+  }
+  filename := string(p[2:index])
+  return filename, nil
 }
 
 func makeWRQ(filename string) ([]byte, error){
@@ -41,7 +91,8 @@ func makeWRQ(filename string) ([]byte, error){
   packet := []byte{0,2}
   packet = append(packet, []byte(filename)...)
   packet = append(packet, []byte{0}...)
-  packet = append(packet, []byte("octet0")...)
+  packet = append(packet, []byte("octet")...)
+	packet = append(packet, []byte{0}...)
   return packet, nil
 }
 
@@ -52,35 +103,77 @@ func SendWRQ(filename string, Conn *net.UDPConn){
   checkError(err)
 }
 
-SendWRQTo(filename string, Conn *net.UDPConn){
+func SendWRQTo(filename string, Conn *net.UDPConn, raddr *net.UDPAddr){
   msg, err := makeWRQ(filename)
   checkError(err)
-  _,err = Conn.WriteTo(msg)
+  _,err = Conn.WriteTo(msg, raddr)
   checkError(err)
 }
 
-func makeDATA(block byte, data []byte) ([]byte, error){
+func HandleWRQ(filename string, in <-chan []byte, Conn *net.UDPConn, raddr *net.UDPAddr, outf chan<- *File){
+
+	//TODO timeout
+	SendACKTo([]byte {0, 0}, Conn, raddr) //send initial confirmation
+	temp := []byte{}
+	for r := range in {
+		block, data, err := GetData(r)
+		checkError(err)
+		SendACKTo(block, Conn, raddr)
+		temp = append(temp, data...)
+		if(len(data) < 512){
+			break
+		}
+	}
+	outf <- &File{filename, temp, Conn, raddr}
+}
+
+func WriteFile(in <-chan *File, m map[string][]byte){
+	for r := range in {
+		_,present := m[r.Name]
+		if(!present) {
+			m[r.Name] = r.Data
+		}else{
+			errCode := []byte {0, 6}
+			errMsg  := "No overwriting allowed"
+			SendERRORTo(errCode, errMsg, r.Conn, r.Raddr)
+		}
+	}
+} 
+
+func GetWRQname(p []byte) (string, error){ 
+	if(len(p) < 9){ //opcode + 0 + octet + 0
+		return "", errors.New("badly formed WRQ packet")
+	}
+	index := 0
+	for i := 2; i < len(p); i++ {
+		if p[i] == 0 { index = i; break  }
+	}
+	filename := string(p[2:index])
+	return filename, nil
+}
+
+func makeDATA(block []byte, data []byte) ([]byte, error){
   //range of block size for file: 0- 255
 	if(len(data) > 512){
 		return nil, errors.New("data must be 512 bytes or fewer")
 	}
   packet := []byte{0,3}
-  packet = append(packet, []byte{block}...)
+  packet = append(packet, block...)
   packet = append(packet, data...)
   return packet, nil
 }
 
-func SendDATA(block byte, data []byte, Conn *net.UDPConn){
+func SendDATA(block []byte, data []byte, Conn *net.UDPConn){
 	msg, err := makeDATA(block, data)
 	checkError(err)
 	_,err = Conn.Write(msg)
 	checkError(err)
 }
 
-SendDATATo(block byte, data []byte, Conn *net.UDPConn){
+func SendDATATo(block []byte, data []byte, Conn *net.UDPConn, raddr *net.UDPAddr){
   msg, err := makeDATA(block, data)
   checkError(err)
-  _,err = Conn.WriteTo(msg)
+  _,err = Conn.WriteTo(msg, raddr)
   checkError(err)
 }
 
@@ -94,22 +187,23 @@ func GetData(p []byte) ([]byte, []byte, error){
 	return block, data, nil
 }
 
-func makeACK(block byte) ([]byte, error){
-  packet := []byte{0,4,block}
+func makeACK(block []byte) ([]byte, error){
+  packet := []byte{0,4}
+	packet = append(packet, block...)
   return packet, nil
 }
 
-func SendACK(block byte, Conn *net.UDPConn){
+func SendACK(block []byte, Conn *net.UDPConn){
 	msg, err := makeACK(block)
 	checkError(err)
 	_,err = Conn.Write(msg)
 	checkError(err)	
 }
 
-SendACKTo(block byte, Conn *net.UDPConn){
+func SendACKTo(block []byte, Conn *net.UDPConn, raddr *net.UDPAddr){
   msg, err := makeACK(block)
   checkError(err)
-  _,err = Conn.WriteTo(msg)
+  _,err = Conn.WriteToUDP(msg, raddr)
   checkError(err)
 }
 
@@ -139,10 +233,10 @@ func SendERROR(errCode []byte, errMsg string, Conn *net.UDPConn){
 	checkError(err)
 }
 
-func SendERRORTo(errCode []byte, errMsg string, Conn *net.UDPConn){
+func SendERRORTo(errCode []byte, errMsg string, Conn *net.UDPConn, raddr *net.UDPAddr){
   msg, err := makeERROR(errCode, errMsg)
   checkError(err)
-  _,err = Conn.WriteTo(msg)
+  _,err = Conn.WriteTo(msg, raddr)
   checkError(err)
 }
 
